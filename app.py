44298ffac6f6043246e33b8a2e861bfa3c5def55
krissy <kmujakic@gmail.com>
2023-04-30 21:22:02 +0200

    handle logout

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -115,6 +115,10 @@ def logout():
     """Handle logout of user."""
 
     # IMPLEMENT THIS
+    do_logout()
+
+    flash(f"Goodbye!", "success")
+    return redirect('/login')
 
 
 ##############################################################################
